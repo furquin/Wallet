631c21011ddb4e4fa8bd429a1d10e8e2fddc238e
furquin <laert.ff@gmail.com>
2022-01-10 16:44:14 -0300

    cria actions e actions types

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,12 @@
-// Coloque aqui suas actions
+export const USER_INFO = 'USER_INFO';
+export const WALLET_INFO = 'WALLET_INFO';
+
+export const saveInfoUser = (user) => ({
+  type: USER_INFO,
+  user,
+});
+
+export const saveWalletInfo = (wallet) => ({
+  type: WALLET_INFO,
+  wallet,
+});
