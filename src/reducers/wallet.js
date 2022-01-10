282ff5c48067287d527e8970a15095cc6b443888
furquin <laert.ff@gmail.com>
2022-01-10 16:45:41 -0300

    cria initial state e recuder wallet

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,21 @@
-// Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+import { WALLET_INFO } from '../actions/index';
+
+const INITIAL_STATE = {
+  wallet: {
+    currencies: [],
+    expenses: [],
+  },
+};
+
+const wallet = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case WALLET_INFO: {
+    return { ...state, user: action.saveWalletInfo };
+  }
+
+  default:
+    return state;
+  }
+};
+
+export default wallet;
