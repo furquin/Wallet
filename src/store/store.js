da00ec6b9c088ce2f602529d8d94af62b78e4da6
furquin <laert.ff@gmail.com>
2022-01-18 16:02:16 -0300

    feat: implementa applyMiddleware

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,8 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
 import rootReducers from '../reducers/index';
 
-const store = createStore(rootReducers, composeWithDevTools());
+const store = createStore(rootReducers, composeWithDevTools(applyMiddleware(thunk)));
 
 export default store;
