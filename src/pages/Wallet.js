e4cc04c2f7835969ade56902b3042e3f512fcfc0
furquin <laert.ff@gmail.com>
2022-01-18 12:55:39 -0300

    feat: renderiza tabela na page wallet

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import Table from '../components/Table';
 import UserWallet from '../components/UserWallet';
 
 class Wallet extends React.Component {
   render() {
     return (
       <div>
-
         <UserWallet />
+        <Table />
 
       </div>);
   }
