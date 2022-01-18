a971e05c6e138be42524046d1eb3f34046782e4a
furquin <laert.ff@gmail.com>
2022-01-18 16:00:08 -0300

    feat: implementa thunks

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,34 @@
+import fetchApi from '../services/walletAPI';
+
 export const USER_INFO = 'USER_INFO';
 export const WALLET_INFO = 'WALLET_INFO';
+export const CURRENCY = 'CURRENCY';
 
 export const saveInfoUser = (email) => ({
   type: USER_INFO,
   email,
 });
 
-export const saveWalletInfo = (wallet) => ({
+export const saveWalletInfo = (expenses, currencies) => ({
   type: WALLET_INFO,
-  wallet,
+  expenses,
+  currencies,
 });
+
+export const saveCurrencyInfo = (currencies) => ({
+  type: CURRENCY,
+  currencies,
+});
+
+export const saveWalletInfoThunk = (expenses) => async (dispatch) => {
+  const currencies = await fetchApi();
+  dispatch(saveWalletInfo(expenses, currencies));
+};
+
+export const saveCurrencyInfoThunk = () => async (dispatch) => {
+  const ApiResult = await fetchApi();
+  const objectApi = Object.keys(ApiResult);
+  const USDT = objectApi.indexOf('USDT');
+  objectApi.splice(USDT, 1);
+  dispatch(saveCurrencyInfo(objectApi));
+};
