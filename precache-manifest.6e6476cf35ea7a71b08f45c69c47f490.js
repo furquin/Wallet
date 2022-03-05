566c81e492cd7b16cdce29dceccc0a515b36fdc7
furquin <laert.ff@gmail.com>
2022-03-05 15:26:09 -0300

    Updates

--- precache-manifest.6e6476cf35ea7a71b08f45c69c47f490.js
+++ precache-manifest.6e6476cf35ea7a71b08f45c69c47f490.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f39e6d49c4e4cce191205373c7c53528",
+    "revision": "7b0d8ed6ecd8115c6ad1f2562fc63701",
     "url": "./index.html"
   },
   {
-    "revision": "3f68b1ece0ec74f709c2",
+    "revision": "eec4453ac2e6773a91a4",
     "url": "./static/css/main.5ecd60fb.chunk.css"
   },
   {
-    "revision": "c4e871b55b8a13aad3b6",
-    "url": "./static/js/2.e5a14b11.chunk.js"
+    "revision": "09637aa5bd8ef9c0a9f2",
+    "url": "./static/js/2.6d0f0988.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "./static/js/2.e5a14b11.chunk.js.LICENSE.txt"
+    "url": "./static/js/2.6d0f0988.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "3f68b1ece0ec74f709c2",
-    "url": "./static/js/main.60a019ba.chunk.js"
+    "revision": "eec4453ac2e6773a91a4",
+    "url": "./static/js/main.7a4867c0.chunk.js"
   },
   {
     "revision": "6ef1c7e4f51c36eaf084",
